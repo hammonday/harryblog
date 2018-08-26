1cbc97af72f0370020c1ebd5082784cca1c187be
harrycharleshammond <harrycharleshammond@hotmail.com>
2018-08-26 09:41:00 -0400

    create homepage and add links

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'pages#home'
   get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+ 
   
   resources :articles
 
